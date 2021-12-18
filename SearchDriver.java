1f9a1745916661aa8ff5ccf34a9cc4e640986a28
Julia Kozak <jkozak30@stuy.edu>
2021-12-18 13:47:38 -0500

    driver

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,27 +1,24 @@
 public class SearchDriver {
-	static Comparable[][] a;
+	static Comparable[] a;
 	static long initTime, midTime, finalTime;
 	static int binTime, linTime, target;
 
 	public static String compareTime(int numTrials, int length) {
 		String result;
-		a = new Comparable[numTrials][];
+		a = new Comparable[length];
 		for (int i=0; i<a.length; i++) {
-			a[i] = new Comparable[length];
-			for (int j=0; j<a[i].length; j++) {
-				a[i][j] = j;
+			a[i] = i;
 			}
-		}
 
 		initTime = System.currentTimeMillis();
-		for (Comparable[] n : a) {
+		for (int i=0; i<numTrials; i++) {
 			target = (int)(Math.random() * length);
-			BinSearch.binSearch(n, target);
+			BinSearch.binSearch(a, target);
 		}
 		midTime = System.currentTimeMillis();
-		for (Comparable[] n : a) {
+		for (int i=0; i<numTrials; i++) {
 			target = (int)(Math.random() * length);
-			LinSearch.linSearch(n, target);
+			LinSearch.linSearch(a, target);
 		}
 		finalTime = System.currentTimeMillis();
 
@@ -49,9 +46,8 @@ public class SearchDriver {
 		System.out.println(compareTime(5000, 100));
 		System.out.println(compareTime(5000, 1000));
 		System.out.println(compareTime(5000, 10000));
-		System.out.println();
-		System.out.println(compareTime(500, 50000));
-		System.out.println(compareTime(500, 100000));
+		System.out.println(compareTime(5000, 100000));
+		System.out.println(compareTime(5000, 1000000));
 
 
 	}
